d5ed03d75a20669c31acc78301c0b2a1c1d4086e
XDM94 <sturmtrooper94@gmail.com>
2024-04-17 20:41:49 +0300

    model prep change

--- HM_1/model_preparation.py
+++ HM_1/model_preparation.py
@@ -1,6 +1,6 @@
 import joblib
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LassoLarsIC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
@@ -13,7 +13,7 @@ y = data['temperature']
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=1, random_state=42)
 
 # Создание и обучение модели
-model = LinearRegression()
+model = LassoLarsIC()
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_train)

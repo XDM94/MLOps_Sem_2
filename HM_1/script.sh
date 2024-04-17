51f032be30c483a113aea1e1ea65d2653340b944
XDM94 <sturmtrooper94@gmail.com>
2024-04-17 18:42:03 +0300

    Change script.sh

--- HM_1/script.sh
+++ HM_1/script.sh
@@ -1,9 +1,9 @@
 echo "----Create Dataset (begin)-----"
-python3 /home/ubuntu/project/create_dataset.py
+python3 /home/sturmtrooper/PycharmProjects/MLOps_Sem_2/HM_1/create_dataset.py
 echo "----Create Dataset (end)-----"
 echo "----Train the Model (begin)-----"
-python3 /home/ubuntu/project/train_model.py
+python3 /home/sturmtrooper/PycharmProjects/MLOps_Sem_2/HM_1/train_model.py
 echo "----Train the Model (end)-----"
 echo "----Use the Model for Prediction (begin)-----"
-python3 /home/ubuntu/project/prediction.py
+python3 /home/sturmtrooper/PycharmProjects/MLOps_Sem_2/HM_1/make_prediction.py
 echo "----Use the Model for Prediction (begin)-----"

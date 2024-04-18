e4bc4038745244ac05b0393a1a4c204a1a60fa3e
XDM94 <sturmtrooper94@gmail.com>
2024-04-18 18:24:01 +0300

    Add new homework

--- HM_1/pipeline.sh
+++ HM_1/pipeline.sh
@@ -1,7 +1,3 @@
-python3 -m venv /home/sturmtrooper/homework1/ || { echo "Ошибка при создании виртуального окружения."; exit 1; }
-source /home/sturmtrooper/homework1/bin/activate || { echo "Ошибка при активации виртуального окружения."; exit 1; }
-echo "Устанавливаю зависимости"
-pip install -r /home/sturmtrooper/PycharmProjects/MLOps_Sem_2/HM_1/requirements.txt || { echo "Ошибка при установке зависимостей."; exit 1; }
 echo "----Create Dataset (begin)-----"
 python3 /home/sturmtrooper/PycharmProjects/MLOps_Sem_2/HM_1/data_creation.py
 echo "----Create Dataset (end)-----"
